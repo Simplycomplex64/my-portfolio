611251f93822c3b7aa6c0d9f3e445bf6eac6e557
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-09-08 17:17:03 -0400

    well i hope it works this time

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        project1: resolve(__dirname, 'project-1.html'),
+        project2: resolve(__dirname, 'project-2.html'),
+        project3: resolve(__dirname, 'project-3.html'),
+        project4: resolve(__dirname, 'project-4.html'),
+        project5: resolve(__dirname, 'project-5.html'),
+        project6: resolve(__dirname, 'project-6.html'),
+        project7: resolve(__dirname, 'project-7.html'),
+        project8: resolve(__dirname, 'project-8.html'),
+      }
+    }
+  }
+});
